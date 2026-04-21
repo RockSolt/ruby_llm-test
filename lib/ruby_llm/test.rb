ecdf4cbc96ec30826988a18cdfd7b28e790c9b73
Todd Kummer <todd@rockridgesolutions.com>
2026-04-21 15:13:51 -0700

    bundle gem ruby_llm-test --test=minitest --mit

---
+++ lib/ruby_llm/test.rb
@@ -0,0 +1,8 @@
+require "ruby_llm/test/version"
+
+module RubyLlm
+  module Test
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby_llm/test/version.rb
@@ -0,0 +1,5 @@
+module RubyLlm
+  module Test
+    VERSION = "0.1.0"
+  end
+end
